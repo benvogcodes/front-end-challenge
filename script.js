148e931921287f9f5c3bf980799c18bf361dbe42
benvogcodes <benjamin.voglewede@gmail.com>
2016-09-04 20:15:57 -0700

    Add .col-xs-12 to product-container; Make build updatehtml method build 1 large row.

--- script.js
+++ script.js
@@ -31,12 +31,12 @@ function domobj(){
 
   self.updatedom = function(){
     var i=0
-    thishtml='';
+    thishtml="<div class='row-fluid'>";
+
     for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row-fluid'>"; console.log("START") }
       thishtml += self.products[i].htmlview;
-      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
     }
+    thishtml += "</div>"
     $("#content").append(thishtml)
   }
 
